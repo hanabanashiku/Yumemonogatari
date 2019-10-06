b63ed13565d532a131e124fea654b1d9e747347d
Michael MacLean <maclm96@protonmail.com>
2019-10-05 21:26:49 -0400

    Player motion and Health

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Character : MonoBehaviour {
+
+    private int _health;
+    protected Animator Animator;
+    protected SpriteRenderer Sprite;
+
+    /// <summary>
+    /// The character's current health.
+    /// </summary>
+    public int Health {
+        get => _health;
+        set {
+            if(value < 0)
+                _health = 0;
+            else if(value > maxHealth)
+                _health = maxHealth;
+            else 
+                _health = value;
+        }
+    }
+
+    /// <summary>
+    /// The character's maximum health.
+    /// </summary>
+    public int maxHealth;
+
+    protected const float WalkSpeed = 3.0f;
+    protected const float RunSpeed = 8.0f;
+
+    protected virtual void Start() {
+        Animator = gameObject.GetComponent<Animator>();
+        Sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+}
---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerCharacter : Character {
+
+    private Vector2 _direction = Vector2.up;
+    private Sprite[] _sprites;
+    private static readonly int Left = Animator.StringToHash("Left");
+    private static readonly int Right = Animator.StringToHash("Right");
+    private static readonly int Forward = Animator.StringToHash("Forward");
+    private static readonly int Backwards = Animator.StringToHash("Backwards");
+    private static readonly int Stop = Animator.StringToHash("Stop");
+
+    protected override void Start() {
+        base.Start();
+        _sprites = Resources.LoadAll<Sprite>("MainCharacter");
+        Animator.enabled = false;
+    }
+    
+    void Update() {
+        Vector2 pos = transform.position;
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        var d = GetDirection(h, v);
+
+        // changing direction
+        // todo: start animation, change sprite
+        if(d != _direction) {
+
+            if(d == Vector2.left) {
+                Animator.enabled = true;
+                Animator.SetTrigger(Left);
+            }
+            else if(d == Vector2.right) {
+                Animator.enabled = true;
+                Animator.SetTrigger(Right);
+                
+            }
+            else if(d == Vector2.up) {
+                Animator.enabled = true;
+                Animator.SetTrigger(Forward);
+            }
+
+            else if(d == Vector2.down) {
+                Animator.enabled = true;
+                Animator.SetTrigger(Backwards);
+            }
+            else if (d == Vector2.zero) {
+                Animator.SetTrigger(Stop);
+                Animator.enabled = false;
+                // what was the last direction?
+                if(_direction == Vector2.left)
+                    Sprite.sprite = _sprites[8];
+                else if(_direction == Vector2.right)
+                    Sprite.sprite = _sprites[11];
+                else if(_direction == Vector2.up)
+                    Sprite.sprite = _sprites[2];
+                else if(_direction == Vector2.down)
+                    Sprite.sprite = _sprites[5];
+            }
+            else {
+                Debug.Log($"Unexpected {d}!");
+            }
+            _direction = d;
+        }
+
+        var speed = Input.GetButton("Sprint") ? RunSpeed : WalkSpeed;
+        pos += speed * d * Time.deltaTime;
+        transform.position = pos;
+    }
+    
+    // Get the current direction based on input axis
+    protected Vector2 GetDirection(float h, float v) {
+        if(Mathf.Abs(h) < 0.05f && Mathf.Abs(v) < 0.05f)
+            return Vector2.zero;
+        if(Mathf.Abs(h) > Mathf.Abs(v)) 
+            return new Vector2(Mathf.Sign(h), 0);
+       
+        return new Vector2(0, Mathf.Sign(v));
+    }
+}
